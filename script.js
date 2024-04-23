7dbfb1a3d18672676e77fbbe5ed8fedfaf8bf428
Castoxity Sama <sameerrajchal@gmail.com>
2024-04-23 15:55:59 +0545

    updated the video in back

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 const slides = document.querySelectorAll('#slideshow .slide');
 let currentSlide = 0;
 
@@ -18,3 +17,23 @@ function nextSlide() {
 }
 
 setInterval(nextSlide, 3000); // Change slide every 3 seconds (3000 milliseconds)
+
+const bgVideo = document.getElementById('bg-video');
+
+window.addEventListener('scroll', () => {
+    if (isElementInViewport(bgVideo)) {
+        bgVideo.play();
+    } else {
+        bgVideo.pause();
+    }
+});
+
+function isElementInViewport(el) {
+    const rect = el.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
